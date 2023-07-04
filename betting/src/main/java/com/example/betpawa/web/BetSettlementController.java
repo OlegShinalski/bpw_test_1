540a7c5bd93250a9ccaf7a4bc8692b73b7ff8d56
OlegShinalski <o.shinalski@gmail.com>
2023-07-04 09:19:45 +0300

    cleaning

--- betting/src/main/java/com/example/betpawa/web/BetSettlementController.java
+++ betting/src/main/java/com/example/betpawa/web/BetSettlementController.java
@@ -1,16 +1,13 @@
 package com.example.betpawa.web;
 
+import com.example.betpawa.model.SettleBetItemDto;
+import com.example.betpawa.model.SettleStatus;
+import com.example.betpawa.service.BetSettlementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.betpawa.model.SettleBetItemDto;
-import com.example.betpawa.model.SettleStatus;
-import com.example.betpawa.service.BetService;
-import com.example.betpawa.service.BetSettlementService;
-
-import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
